fc192ebe67f93583d2d7b81a70c18c51b61e9c1d
Konstantin <pkv25011990@gmail.com>
2023-08-09 17:36:31 +0500

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        for (int i = 0; i < 10; i++)
+            tree.add(new Random().nextInt(20));
+        tree.print();
+    }
+}
